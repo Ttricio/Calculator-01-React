5368515af0f2448128383c943435748d76f602b5
Patryk Kaczmarek <patrykmkaczmarek@gmail.com>
2022-10-11 09:41:37 +0200

    css styling done

--- src/App.js
+++ src/App.js
@@ -1,41 +1,39 @@
 import "./App.css"
 
+
 function App() {
-  // this function creates digits from 1-9 we are just pushing new numbers into the array named digits. We could of course use regular buttons in html structure but function can to it instead.
+	// this function creates digits from 1-9 we are just pushing new numbers into the array named digits. We could of course use regular buttons in html structure but function can to it instead.
 	const createDigits = () => {
 		const digits = []
 		for (let i = 1; i < 10; i++) {
-digits.push(
-  <button key={i}>{i}</button>
-)
-    }
-    return digits;
-  }
+			digits.push(<button key={i}>{i}</button>)
+		}
+		return digits
+	}
 	// this is regular html structur of course created with react syntax
 	return (
 		<div className='App'>
-			<div clasName='calculator'>
+			<div className='calculator'>
 				<div className='display'>
 					<span>(0)</span>0
 				</div>
-				<div clasName='operatots'>
+				<div className='operators'>
 					<button>/</button>
 					<button>*</button>
 					<button>+</button>
 					<button>-</button>
 
 					<button>DEL</button>
+				</div>
 					<div className='digits'>
-            {createDigits()}
+						{createDigits()}
 						<button>0</button>
 						<button>.</button>
 						<button>=</button>
 					</div>
-				</div>
 			</div>
 		</div>
 	)
 }
-
 
 export default App
